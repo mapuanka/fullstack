c96592fab0efa2c2f69cde05047cefe684c6f44a
mapuanka <marianna.chekurova@gmail.com>
2019-06-07 20:18:16 +0300

    Kurssitiedot -tehtävä suoritettu

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,77 +1,67 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) =>{
-    return(
+const Header = (props) => {
+    return (
         <div>
-            <h1>
-                {props.course}
-            </h1>
+            <h1>{props.name}</h1>
         </div>
+
     )
-
 }
-
 const Content = (props) => {
-    return(
-        <div>
-            <p> 
-                <Part name={props.parts[0].name} exercise={props.parts[0].exercises} />
-                <Part name={props.parts[1].name} exercise={props.parts[1].exercises} />
-                <Part name={props.parts[2].name} exercise={props.parts[2].exercises} />
-            </p>
+    return (
+         <div>
+            <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+            <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+            <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
         </div>
-    )
 
+    )
 }
 
-const Part = (props) =>{     
+const Part = (props) => {
     return(
-        <div>
-            <p> 
-                {props.name} {props.exercise}
-            </p>
+        <div>  
+             <p>{props.part} {props.exercise}</p>
         </div>
     )
-
 }
-
-const Total =(props) => {
-    return(
+const Total = (props) => {
+    return (
         <div>
-            <p>
-            yhteensä {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} tehtävää
-            </p>
+            <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} </p>
         </div>
-    )
 
+    )
 }
 
 const App = () => {
     const course = {
-      name: 'Half Stack -sovelluskehitys',
-      parts: [
-        {
-          name: 'Reactin perusteet',
-          exercises: 10
-        },
-        {
-          name: 'Tiedonvälitys propseilla',
-          exercises: 7
-        },
-        {
-          name: 'Komponenttien tila',
-          exercises: 14
-        }
-      ]
-    }
-return (
-    <>
-    <Header course={course.name} />
-    <Content parts={course.parts}/>
-    <Total parts={course.parts}/>
-    </>
-    )
+        name: 'Half Stack application development',
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7
+          },
+          {
+            name: 'State of a component',
+            exercises: 14
+          }
+        ]
+      }
+
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,34 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
-    return (
-      <div>
-        <p>Hello {props.name}, your are {props.age} years old</p>
-      </div>
-    )
-  }
-  
-  const App = () => {
-    const name = 'Pekka'
-    const age = 10
-  
-    return (
-      <>
-        <h1>Greetings</h1>
-        <Hello name="Maya" age={26 + 10} />
-        <Hello name={name} age={age} />
-        <Footer />
-      </>
-    )
-  }
-  const Footer = () => {
-    return (
-      <div>
-        greeting app created by 
-        <a href="https://github.com/mapuanka">mapuanka</a>
-      </div>
-    )
+const Display = props => <div> {props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = (props) => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = (amount) => {
+    setValue(amount)
   }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+  return (
+    <div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(value+1)} text='increase' />
+      <Button handleClick={() => setToValue(0)} text='zero' />
+      <Button handleClick={() => setToValue(value-1)} text='decrease' />
+    </div>
+  )
+}
+
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
